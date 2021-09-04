845292b2113852f793e40fdf4852f31654c08366
Oluwaseyi Water <>
2021-09-04 19:12:49 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace projectvscode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int radius;
+            double diameter;
+            double circumference;
+            double area;
+
+            Console.WriteLine("Enter a value for radius:");
+            radius = Convert.ToInt32(Console.ReadLine());
+            diameter = 2 * radius;
+            circumference = 2 * radius * Math.PI ;
+            area = radius * radius * Math.PI ;
+
+            Console.WriteLine("Diameter of the circle is: {0}", diameter);
+            Console.WriteLine("Circumference of the circle is: {0}", circumference);
+            Console.WriteLine("Area of the circle is: {0}", area);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# projectvscode
+ Frontend
